46afe8f936c6ae1d0de7b1a8c85c4d933ef4ff8e
Julia Gao Miller <julia.gao.miller@gmail.com>
2017-12-23 23:44:40 -0800

    child to parent component communication

--- src/components/UserDetail.vue
+++ src/components/UserDetail.vue
@@ -3,6 +3,7 @@
         <h3>You may view the User Details here</h3>
         <p>Many Details</p>
         <p>Username: {{ switchName() }} </p>
+        <button @click="resetName()">Reset name</button>
     </div>
 </template>
 
@@ -17,6 +18,10 @@
     methods: {
       switchName(){
         return this.name.split("").reverse().join("");
+      },
+      resetName(){
+        this.name = "Max";
+        this.$emit('nameWasReset', this.name);
       }
     }
   }
